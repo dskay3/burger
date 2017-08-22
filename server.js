202778d8f01228987b13b2fc894b92fb978d570a
Daniel <dskay3@gmail.com>
2017-08-22 01:29:24 -0400

    Established ES6 notation and added code to import routes, override with POST, and establish express instance.

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 // NPM packages
-var express = require('express');
-var override = require('method-override');
-var bodyParser = require('body-parser');
+const express = require('express');
+const override = require('method-override');
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
+// Establish port
+const PORT = 8000 || process.env.PORT;
 
+// Establish express instance
+const app = express();
 
+// Serve static content for the app from 'public' directory in the application directory
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(override("_method"));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access
+const routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+app.listen(PORT, () => {
+  console.log('App is listening on ' + PORT);
+});
